1e2dc0792247ed5e94f6f77ff0b495cde12e3969
Alexander Litvinenko <alekslitvinenk@gmail.com>
2023-10-06 16:51:38 +0300

    Final brushes, fix skipping the right side of triangle

--- src/main/scala/com/alekslitvinenk/triagngles/Main.scala
+++ src/main/scala/com/alekslitvinenk/triagngles/Main.scala
@@ -1,34 +1,42 @@
 package com.alekslitvinenk.triagngles
 
 import cats.data.NonEmptyList
-import com.alekslitvinenk.triagngles.domain.Node
+import com.alekslitvinenk.triagngles.domain.Calc
 
 import scala.io.Source
 
 object Main extends App {
   
   private val source = Source.fromInputStream(System.in)
-  
   private val lines = source.getLines().takeWhile(p => p.nonEmpty && p != "\n").toList
-  private val rootNodeVal = lines.head.toInt
-  private val previous = Array(Node(NonEmptyList(rootNodeVal, List.empty)))
+  private val rootNodeVal = lines.head.trim.toInt
+  private val previous = Array(Calc(NonEmptyList(rootNodeVal, List.empty)))
   
-  // calculating triangle by taking a line at time and converting it into nodes and appending parent node path's
+  // calculating triangle by taking a line at time and converting it into Calc paths and appending parent Calc path
   val tree = lines.tail.foldLeft(previous) { (prev, line) =>
+    // grouping 'prev' Calc paths by original nodes
+    val prevOrigNodes = {
+      val middle = prev.tail.dropRight(1).grouped(2).toArray
+      middle.prepended(Array(prev.head)).appended(Array(prev.last))
+    }
     // row of integers from consumed line
-    val row = line.split(" ").map(_.toInt)
+    val row = line.split(" ")
+      .map(_.trim)
+      .map(_.toInt)
     
     // pairs of integers, each pair represents leaf nodes for corresponding row of parent row 'prev'
     val pairs = row.zip(row.tail)
-
-    // then 'prev' row is zipped with pairs of numbers from current row 'row'
-    prev.zip(pairs).flatMap { o =>
-      // and here we produce a new row of Nodes
-      List(
-        Node(o._1.path :+ o._2._1),
-        Node(o._1.path :+ o._2._2),
-      )
+    
+    val solutions = prevOrigNodes.zip(pairs).flatMap { o =>
+      o._1.flatMap { e =>
+        List(
+          Calc(e.path :+ o._2._1),
+          Calc(e.path :+ o._2._2),
+        )
+      }
     }
+    
+    solutions
   }
   
   val rezNode = tree.minBy(_.sum)
--- src/main/scala/com/alekslitvinenk/triagngles/domain/Calc.scala
+++ src/main/scala/com/alekslitvinenk/triagngles/domain/Calc.scala
@@ -2,7 +2,7 @@ package com.alekslitvinenk.triagngles.domain
 
 import cats.data.NonEmptyList
 
-case class Node(path: NonEmptyList[Int]) {
+case class Calc(path: NonEmptyList[Int]) {
   val value: Int = path.head
   val sum: Int = path.toList.sum
 }
